b3a196bd02b784a7fd1eaf2d281415b34385adc0
gab0g0nzalez <gf.gonzalez@duocuc.cl>
2026-06-21 23:58:57 -0400

    V5 Arreglo funciones swagger, ingreso de Servidor Eureka y snapshot de microservicios.

--- centro_acopio-service/src/main/java/com/donaton/centro_acopio_service/config/SwaggerConfig.java
+++ centro_acopio-service/src/main/java/com/donaton/centro_acopio_service/config/SwaggerConfig.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenApi(){
-        return new OpenAPI().info(new Info().title("API de Centro de Acopio").version("1.0").description("Microservicio para la gestion Centros de Acopio de DonatON"));
+        return new OpenAPI().info(new Info().title("API Gestion de los Beneficiarios").version("1.0").description("Microservicio para la gestion de los Centro Acopio de DonatON"));
     }
 }
--- distribucion-service/src/main/java/com/donaton/distribucion_service/DistribucionServiceApplication.java
+++ distribucion-service/src/main/java/com/donaton/distribucion_service/DistribucionServiceApplication.java
@@ -2,8 +2,10 @@ package com.donaton.distribucion_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DistribucionServiceApplication {
 
 	public static void main(String[] args) {
